d4622de65badac906f90d623211543c1af4ec41a
Suvendu Chowdhury <105709770+SUVENDUC789@users.noreply.github.com>
2022-11-26 16:45:10 +0530

    Recaptcha add successfully

--- joinourteamapp/views.py
+++ joinourteamapp/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
 from .models import JoinOurTeam
+import requests
 # Create your views here.
 
 def jointeam(request):
     if request.method == "POST":
-        fname=request.POST.get('fname','default')
-        lname=request.POST.get('lname','default')
-        email=request.POST.get('email','default')
-        phone=request.POST.get('phone','default')
-        Position=request.POST.get('Position','default')
+        # recaptcha stuff 
+        clientkey=request.POST.get('g-recaptcha-response','default')
+        secretkey='6LdbcDQjAAAAAFNRRStnvGPDImFq2Ul8pGWYGApO'
+        captchaData={
+            'secret':secretkey,
+            'response':clientkey
+        }
+        r=requests.post(' https://www.google.com/recaptcha/api/siteverify',data=captchaData)
+        r=r.json()
 
-        en=JoinOurTeam(fname=fname,lname=lname,email=email,phone=phone,Position=Position)
-        en.save()
+        if r['success']:
+            fname=request.POST.get('fname','default')
+            lname=request.POST.get('lname','default')
+            email=request.POST.get('email','default')
+            phone=request.POST.get('phone','default')
+            Position=request.POST.get('Position','default')
+
+            en=JoinOurTeam(fname=fname,lname=lname,email=email,phone=phone,Position=Position)
+            en.save()
+            p={
+                'l':  r['success']
+                }
+            return render(request,'joinourteamapp/jot.html',p)
     return render(request,'joinourteamapp/jot.html')
--- schcontactapp/views.py
+++ schcontactapp/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
 from .models import SchoolContact
+import requests
 # Create your views here.
 
 
 def schcontact(request):
     if request.method == "POST":
-        name=request.POST.get('name','default')
-        email=request.POST.get('email','default')
-        phone=request.POST.get('phone','default')
-        about=request.POST.get('about','default')
+        clientkey=request.POST.get('g-recaptcha-response','default')
+        secretkey='6LdbcDQjAAAAAFNRRStnvGPDImFq2Ul8pGWYGApO'
+        captchaData={
+            'secret':secretkey,
+            'response':clientkey
+        }
+        r=requests.post(' https://www.google.com/recaptcha/api/siteverify',data=captchaData)
+        r=r.json()
 
-        en=SchoolContact(name=name,email=email,phone=phone,aboutcourse=about)
-        en.save()
+        if r['success']:
+            name=request.POST.get('name','default')
+            email=request.POST.get('email','default')
+            phone=request.POST.get('phone','default')
+            about=request.POST.get('about','default')
 
+            en=SchoolContact(name=name,email=email,phone=phone,aboutcourse=about)
+            en.save()
+            p={
+                'l':  r['success']
+            }
+            return render(request,'schcontactapp/schcontact.html',p)
         
     return render(request,'schcontactapp/schcontact.html')
